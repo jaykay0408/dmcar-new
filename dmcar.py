4d50514de0b77a7af8e5a80faeb57ee7d3f62fe3
jaykay0408 <jelee0408@gmail.com>
2021-10-13 17:51:36 -0400

    updated

--- dmcar.py
+++ dmcar.py
@@ -6,6 +6,7 @@
 # import the necessary packages
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
+import tensorflow as tf
 from collections import deque
 from imutils.video import VideoStream
 import numpy as np
@@ -52,6 +53,7 @@ MODEL_PATH = "./models/stop_not_stop.model"
 
 # to hide warning message for tensorflow
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 # Start Queues
 show_queue = queue.Queue()
@@ -75,7 +77,10 @@ def main():
     model = load_model(MODEL_PATH)
 
     # Grab the reference to the webcam
-    vs = VideoStream(src=-1).start()
+    #vs = VideoStream(src=-1).start()
+    vs = cv2.VideoCapture(-1)
+    vs.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+    vs.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 
     # detect lane based on the last # of frames
     frame_buffer = deque(maxlen=args["buffer"])
@@ -106,7 +111,7 @@ def main():
     # initialize the total number of frames that *consecutively* contain
     # stop sign along with threshold required to trigger the sign alarm
     TOTAL_CONSEC = 0
-    TOTAL_THRESH = 3            # fast speed-> low, slow speed -> high
+    TOTAL_THRESH = 2            # fast speed-> low, slow speed -> high
     STOP_SEC = 0
     AFTER_STOP_SEC = 0          # skip frames after detection
     STOP = False                # If STOP is detected
@@ -115,7 +120,7 @@ def main():
     # keep looping
     while True:
         # grab the current frame
-        frame = vs.read()
+        ret, frame = vs.read()
         if frame is None:
             break
 
@@ -274,16 +279,17 @@ def main():
             set_dir_servo_angle(ANGLE-90)
         elif keycmd == 'z':
             isMoving = False
-	    SPEED = 0
+            SPEED = 0
             forward(SPEED)
 
     # if we are not using a video file, stop the camera video stream
-    writer.release()
-    vs.stop()
+    if writer is not None:
+        writer.release()
+    vs.release()
 
     # initialize picar
-    #ibw.speed = 0
-    #fw.turn(90)
+    forward(0)
+    set_dir_servo_angle(0)
 
     # close all windows
     cv2.destroyAllWindows()
